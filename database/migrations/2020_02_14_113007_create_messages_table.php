53e5423d631396eb72b121427614eafd683d92af
Shark_vil <sharkvil228@gmail.com>
2020-02-14 14:35:28 +0300

    Создание шалонов объектов переписки.

---
+++ database/migrations/2020_02_14_113007_create_messages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('message');
+            $table->bigInteger('ticket_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('ticket_id')->references('id')->on('tickets');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
